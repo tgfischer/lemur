58de5d7817066aab0142171564594cd5e5295651
tgfischer <tgfischer6@gmail.com>
2023-06-14 19:42:44 -0400

    Scoll to top for comments

--- src/components/PostScreen/index.tsx
+++ src/components/PostScreen/index.tsx
@@ -1,14 +1,16 @@
 import { type NativeStackScreenProps } from "@react-navigation/native-stack";
 import { useQueryClient } from "@tanstack/react-query";
-import { FlatList, Row } from "native-base";
-import { useMemo } from "react";
+import { Row, View } from "native-base";
+import { useMemo, useRef } from "react";
 import {
   ActivityIndicator,
   RefreshControl,
   type ListRenderItem,
+  FlatList,
 } from "react-native";
 
 import { type Tree } from "../../types";
+import { ReturnToTopButton } from "../ReturnToTopButton";
 import { type ScreenType, type FeedStackParamList } from "../types";
 
 import {
@@ -33,6 +35,8 @@ const keyExtractor = (tree: Tree<CommentViewData>): string =>
   tree.value.comment.ap_id;
 
 export const PostScreen = ({ route }: PostScreenProps): JSX.Element | null => {
+  const flatListRef = useRef<FlatList>(null);
+
   const queryClient = useQueryClient();
 
   const postId = route.params.view.post.id;
@@ -49,33 +53,41 @@ export const PostScreen = ({ route }: PostScreenProps): JSX.Element | null => {
   }
 
   return (
-    <FlatList
-      refreshControl={
-        <RefreshControl
-          refreshing={postQuery.isRefetching || commentsQuery.isRefetching}
-          onRefresh={() => {
-            void Promise.all([
-              postQuery.refetch(),
-              commentsQuery.refetch(),
-            ]).then(async () => {
-              await queryClient.resetQueries(queryKeys.getPost(postId));
-              await queryClient.resetQueries(["comments", postId]);
-            });
-          }}
-        />
-      }
-      data={comments}
-      keyExtractor={keyExtractor}
-      renderItem={renderItem}
-      onEndReachedThreshold={0.5}
-      ListHeaderComponent={<PostMasthead view={postQuery.data} />}
-      ListFooterComponent={
-        commentsQuery.isInitialLoading || commentsQuery.isFetchingNextPage ? (
-          <Row p={2} justifyContent="center">
-            <ActivityIndicator />
-          </Row>
-        ) : undefined
-      }
-    />
+    <View>
+      <FlatList
+        ref={flatListRef}
+        refreshControl={
+          <RefreshControl
+            refreshing={postQuery.isRefetching || commentsQuery.isRefetching}
+            onRefresh={() => {
+              void Promise.all([
+                postQuery.refetch(),
+                commentsQuery.refetch(),
+              ]).then(async () => {
+                await queryClient.resetQueries(queryKeys.getPost(postId));
+                await queryClient.resetQueries(["comments", postId]);
+              });
+            }}
+          />
+        }
+        data={comments}
+        keyExtractor={keyExtractor}
+        renderItem={renderItem}
+        onEndReachedThreshold={0.5}
+        ListHeaderComponent={<PostMasthead view={postQuery.data} />}
+        ListFooterComponent={
+          commentsQuery.isInitialLoading || commentsQuery.isFetchingNextPage ? (
+            <Row p={2} justifyContent="center">
+              <ActivityIndicator />
+            </Row>
+          ) : undefined
+        }
+      />
+      <ReturnToTopButton
+        onPress={() =>
+          flatListRef.current?.scrollToOffset({ animated: true, offset: 0 })
+        }
+      />
+    </View>
   );
 };
